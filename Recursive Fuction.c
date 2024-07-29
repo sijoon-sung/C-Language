04874feb198a315755cc2afb625fe9520f811006
sijoon-sung <162120398+sijoon-sung@users.noreply.github.com>
2024-07-29 12:27:17 +0900

    Update Recursive Fuction.c

--- Recursive Fuction.c
+++ Recursive Fuction.c
@@ -19,3 +19,22 @@ int main(void) {
 	scanf("%d", &number);
 	print(number);
 }
+
+
+//  Combination using Recursive Function
+int nCr(int n, int r) {
+	if (r == 0 || r == n) {
+		return 1;
+	}
+	else {
+		return nCr(n - 1, r - 1) + nCr(n - 1, r);
+	}
+}
+
+
+int main(void) {
+	int n, r;
+	scanf("%d %d", &n, &r);
+	printf("%d",nCr(n,r));
+	return 0;
+}
